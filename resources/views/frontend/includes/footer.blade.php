7ade44c9ce3f9dd6c4c031fd46ab2fa70eb59941
mahedi hasan bijoy <bijoycse2014@gmail.com>
2023-07-08 00:47:08 +0600

    footer updated

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="ltn__footer-area  ">
+{{-- <footer class="ltn__footer-area  ">
     <div class="footer-top-area  section-bg-2 plr--5">
         <div class="container-fluid">
             <div class="row">
@@ -66,4 +66,142 @@
             </div>
         </div>
     </div>
+</footer> --}}
+
+<!-- FOOTER AREA START -->
+<footer class="ltn__footer-area  ">
+    <div class="footer-top-area  section-bg-2 plr--5">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-3 col-md-6 col-sm-6 col-12">
+                    <div class="footer-widget footer-about-widget">
+                        <div class="footer-logo">
+                            <div class="site-logo">
+                                <img style="height: 60px;" src="{{asset("frontend/img/kstudio_logo.png")}}" alt="Logo">
+                            </div>
+                        </div>
+                        <p>{{ $footer_data->footer_short_text}}</p>
+                        <div class="footer-address">
+                            <ul>
+                                <li>
+                                    <div class="footer-address-icon">
+                                        <i class="icon-placeholder"></i>
+                                    </div>
+                                    <div class="footer-address-info">
+                                        <p>{{ $contact_data->address}}</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="footer-address-icon">
+                                        <i class="icon-call"></i>
+                                    </div>
+                                    <div class="footer-address-info">
+                                        <p><a href="tel:+0123-456789">{{ $contact_data->phone}}</a></p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="footer-address-icon">
+                                        <i class="icon-mail"></i>
+                                    </div>
+                                    <div class="footer-address-info">
+                                        <p><a href="mailto:{{ $contact_data->email}}">{{ $contact_data->email}}</a></p>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="ltn__social-media mt-20">
+                            <ul>
+                                <li><a href="{{ $footer_data->facebook }}" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="{{ $footer_data->twitter }}" title="Twitter"><i class="fab fa-twitter"></i></a></li>
+                                <li><a href="{{ $footer_data->linkedin }}" title="Linkedin"><i class="fab fa-linkedin"></i></a></li>
+                                <li><a href="{{ $footer_data->youtube }}" title="Youtube"><i class="fab fa-youtube"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-md-6 col-sm-6 col-12">
+                    <div class="footer-widget footer-menu-widget clearfix">
+                        <h4 class="footer-title">Company</h4>
+                        <div class="footer-menu">
+                            <ul>
+                                <li><a href="about.html">About</a></li>
+                                <li><a href="blog.html">Blog</a></li>
+                                <li><a href="shop.html">All Products</a></li>
+                                <li><a href="locations.html">Locations Map</a></li>
+                                <li><a href="faq.html">FAQ</a></li>
+                                <li><a href="contact.html">Contact us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-md-6 col-sm-6 col-12">
+                    <div class="footer-widget footer-menu-widget clearfix">
+                        <h4 class="footer-title">Services</h4>
+                        <div class="footer-menu">
+                            <ul>
+                                <li><a href="order-tracking.html">Order tracking</a></li>
+                                <li><a href="wishlist.html">Wish List</a></li>
+                                <li><a href="login.html">Login</a></li>
+                                <li><a href="account.html">My account</a></li>
+                                <li><a href="about.html">Terms & Conditions</a></li>
+                                <li><a href="about.html">Promotional Offers</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-md-6 col-sm-6 col-12">
+                    <div class="footer-widget footer-menu-widget clearfix">
+                        <h4 class="footer-title">Customer Care</h4>
+                        <div class="footer-menu">
+                            <ul>
+                                <li><a href="login.html">Login</a></li>
+                                <li><a href="account.html">My account</a></li>
+                                <li><a href="wishlist.html">Wish List</a></li>
+                                <li><a href="order-tracking.html">Order tracking</a></li>
+                                <li><a href="faq.html">FAQ</a></li>
+                                <li><a href="contact.html">Contact us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6 col-sm-12 col-12">
+                    <div class="footer-widget footer-newsletter-widget">
+                        <h4 class="footer-title">Newsletter</h4>
+                        <p>Subscribe to our weekly Newsletter and receive updates via email.</p>
+                        <div class="footer-newsletter">
+                            <form action="#">
+                                <input type="email" name="email" placeholder="Email*">
+                                <div class="btn-wrapper">
+                                    <button class="theme-btn-1 btn" type="submit"><i class="fas fa-location-arrow"></i></button>
+                                </div>
+                            </form>
+                        </div>
+                        <h5 class="mt-30">We Accept</h5>
+                        <img src="{{ asset('frontend/img/icons/payment-4.png')}}" alt="Payment Image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="ltn__copyright-area ltn__copyright-2 section-bg-7  plr--5">
+        <div class="container-fluid ltn__border-top-2">
+            <div class="row">
+                <div class="col-md-6 col-12">
+                    <div class="ltn__copyright-design clearfix">
+                        <p>All Rights Reserved @ Company <span class="current-year"></span></p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-12 align-self-center">
+                    <div class="ltn__copyright-menu text-end">
+                        <ul>
+                            <li><a href="#">Terms & Conditions</a></li>
+                            <li><a href="#">Claim</a></li>
+                            <li><a href="#">Privacy & Policy</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
+<!-- FOOTER AREA END -->
